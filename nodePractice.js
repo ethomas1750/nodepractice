ece7d662d332c577c8126fb54c8fcbb913962290
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-06 21:52:21 -0500

    Update nodePractice.js

--- nodePractice.js
+++ nodePractice.js
@@ -1,1 +1,11 @@
+// Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
+const getInput = require("./get-input.js");
+
+let userFirstName = getInput(1)
+let userLastName = getInput(2)
+
+console.log("Hello, " + userFirstName + " " + userLastName) 
+console.log("This is your name capitalized " + userFirstName.toUpperCase() + " " + userLastName.toUpperCase())
+console.log("These are your initials " + userFirstName[0] + ". " + userLastName[0] + ".")
+console.log("Your email address is " + userFirstName[0].toLowerCase() + userLastName.toLowerCase() + ".prsvr@gmail.com")
 
